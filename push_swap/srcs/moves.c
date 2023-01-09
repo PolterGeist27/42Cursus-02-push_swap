b4c566b0731ee6c3c905350a2ad62960f33e8671
Diogo Tavares <diogo.martins.tavares27@gmail.com>
2023-01-09 09:31:18 +0000

    couple functions started

---
+++ push_swap/srcs/moves.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   moves.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diogmart <diogmart@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/05 11:57:31 by diogmart          #+#    #+#             */
+/*   Updated: 2023/01/05 13:14:02 by diogmart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pushswap.h"
+
+/*
+Linked list functions in libft:
+
+	- ft_lstnew(void *content);
+		Allocates (with malloc(3)) and returns a new node.
+		The member variable ’content’ is initialized with the value of the parameter ’content’.
+		The variable ’next’ is initialized to NULL.
+
+	- ft_lstadd_front(t_list **lst, t_list *new);
+		Adds the node ’new’ at the beginning of the list.
+
+	- ft_lstadd_back(t_list **lst, t_list *new);
+		Adds the node ’new’ at the end of the list.
+
+	- ft_lstsize(t_list *lst);
+		Counts the number of nodes in a list.
+
+	- ft_lstlast(t_list *lst);
+		Returns the last node of the list.
+
+	-  ft_lstdelone(t_list *lst, void (*del)(void*));
+		Takes as a parameter a node and frees the memory of the node and it’s content using the function ’del’.
+		The memory of ’next’ is not be freed.
+
+	- ft_lstclear(t_list **lst, void (*del)(void*));
+		Deletes and frees the given node and every successor of that node, using the function ’del’ and free(3).
+		The pointer to the list is set to NULL.
+
+	- ft_lstiter(t_list *lst, void (*f)(void *));
+		Iterates the list ’lst’ and applies the function ’f’ on the content of each node.
+
+	- ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *));
+		Iterates the list ’lst’ and applies the function ’f’ on the content of each node.
+		Creates a new list resulting of the successive applications of the function ’f’.
+		The ’del’ function is used to delete the content of a node if needed.
+*/
+
+/*
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+*/
+
+/*
+	Stack (double pointer) → Head (pointer) → Node (t_list)
+*/
+
+void	swap(t_list **stack)
+{
+	t_list	*tmp;
+
+	tmp = (*stack)->next;
+	(*stack)->next = tmp->next;
+	ft_lstadd_front(stack, tmp);
+//	tmp->next = *stack;
+//	*stack = tmp;
+}
+
+void	push(t_list **stack_give, t_list **stack_receive)
+{
+	ft_lstadd_front(stack_receive, *stack_give);
+	*stack_give = (*stack_give)->next;
+}
+
+void	rotate(t_list **stack)
+{
+	
+}
+
+void	reverse(t_list **stack)
+{
+	
+}
